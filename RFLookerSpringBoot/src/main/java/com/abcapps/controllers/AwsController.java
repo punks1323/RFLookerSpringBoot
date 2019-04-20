6f1e5acbfede66985ac18a5d01a863e7d2bafe37
Pankaj <punks1323@gmail.com>
2019-04-20 09:05:09 +0530

    create topic added

--- RFLookerSpringBoot/src/main/java/com/abcapps/controllers/AwsController.java
+++ RFLookerSpringBoot/src/main/java/com/abcapps/controllers/AwsController.java
@@ -2,7 +2,9 @@ package com.abcapps.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,8 @@ public class AwsController {
     Logger log = LoggerFactory.getLogger(AwsController.class);
 
 
+    @PostMapping("/sendToken")
+    private void saveMobileDeviceToken(@RequestParam("token") String token) {
+
+    }
 }
--- RFLookerSpringBoot/src/main/java/com/abcapps/service/impl/AwsServiceImpl.java
+++ RFLookerSpringBoot/src/main/java/com/abcapps/service/impl/AwsServiceImpl.java
@@ -6,32 +6,39 @@ import com.amazonaws.services.sns.model.CreateTopicRequest;
 import com.amazonaws.services.sns.model.CreateTopicResult;
 import com.amazonaws.services.sns.model.ListTopicsResult;
 import com.amazonaws.services.sns.model.Topic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AwsServiceImpl implements AwsService {
 
+    Logger log = LoggerFactory.getLogger(AwsServiceImpl.class);
     @Autowired
     AmazonSNS amazonSNS;
 
     @Override
     public void createTopicIfNotExists(String topicName) {
         try {
-            //amazonSNS.topic
-            ListTopicsResult listTopicsResult = amazonSNS.listTopics();
-            listTopicsResult.withTopics();
+            boolean isTopicPresent = false;
+            for (Topic topic : amazonSNS.listTopics().getTopics()) {
+                if (topic.getTopicArn().split(":")[5].equals(topicName)) {
+                    isTopicPresent = true;
+                    break;
+                }
+            }
 
-            // create a new SNS topic
-            CreateTopicRequest createTopicRequest = new CreateTopicRequest(topicName);
-            CreateTopicResult createTopicResult = amazonSNS.createTopic(createTopicRequest);
+            if (!isTopicPresent) {
+                // create a new SNS topic
+                CreateTopicRequest createTopicRequest = new CreateTopicRequest(topicName);
+                CreateTopicResult createTopicResult = amazonSNS.createTopic(createTopicRequest);
 
-            // print TopicArn
-            System.out.println(createTopicResult); // get request id for
-
-            // CreateTopicRequest from SNS metadata
-            System.out.println("CreateTopicRequest - " + amazonSNS.getCachedResponseMetadata(createTopicRequest));
-
+                log.info(createTopicRequest.toString());
+                log.info("CreateTopicRequest - " + amazonSNS.getCachedResponseMetadata(createTopicRequest));
+            } else {
+                log.info(topicName + " : topic is already present.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
